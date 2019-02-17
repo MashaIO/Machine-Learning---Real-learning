64baa106d91b3a1343fc781c6d314f9ac6e856eb
venkateshwj <venkat@masha.io>
2019-02-17 16:58:15 +0000

    Intrim check in

--- dl-learnings/RNN/stocks.py
+++ dl-learnings/RNN/stocks.py
@@ -111,4 +111,40 @@ print(xx_train)
 
 
 #%%
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, LSTM
+
+#%%
+regressor = Sequential()
+
+#%%
+# First LSTM layer
+regressor.add(LSTM(units = 50, return_sequences = True, input_shape = (xx_train.shape[1],1)))
+regressor.add(Dropout(0.2))
+
+#%%
+# Second LSTM layer
+regressor.add(LSTM(units = 50, return_sequences = True))
+regressor.add(Dropout(0.2))
+
+#%%
+# Third LSTM layer
+regressor.add(LSTM(units = 50, return_sequences = True))
+regressor.add(Dropout(0.2))
+
+#%%
+# Fourth LSTM layer
+regressor.add(LSTM(units = 50))
+regressor.add(Dropout(0.2))
+
+#%%
+# output layer
+regressor.add(Dense(units = 1))
+
+#%%
+regressor.compile(optimizer = 'adam' , loss = 'mean_squared_error')
+
+#%%
+# fitting 
+regressor.fit(xx_train, yy_train, epochs = 100, batch_size = 32)
 
