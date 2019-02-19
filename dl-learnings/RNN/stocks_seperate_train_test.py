74e31a1e516877579e5c34f26b05d12e9de5bebf
venkateshwj <venkat@masha.io>
2019-02-19 04:50:31 +0000

    Not a proper graph.

--- dl-learnings/RNN/stocks_seperate_train_test.py
+++ dl-learnings/RNN/stocks_seperate_train_test.py
@@ -68,13 +68,55 @@ regressor.add(Dense(units = 1))
 
 regressor.compile(optimizer = 'adam',loss = 'mean_squared_error')
 
-regressor.fit(x_train,y_train,epochs = 100, batch_size = 32)
-regressor.save('model/google_stocks_sep_train_test.h5')
+# regressor.fit(x_train,y_train,epochs = 100, batch_size = 32)
+# regressor.save('model/google_stocks_sep_train_test.h5')
 
 #%%
+from keras.models import load_model
+regressor = load_model('model/google_stocks.h5')
 
 #%%
 dataset_test =pd.read_csv("data/stocks/testset.csv")
 real_stock_price = dataset_test.iloc[:,1:2].values
 dataset_total = pd.concat((dataset_train['Open'],dataset_test['Open']),axis = 0)
 dataset_total
+
+#%%
+inputs = dataset_total[len(dataset_total) - len(dataset_test)-60:].values
+inputs
+
+
+
+#%%
+inputs = inputs.reshape(-1,1)
+
+inputs = sc.transform(inputs)
+inputs.shape
+
+#%%
+x_test = []
+for i in range(60,185):
+    x_test.append(inputs[i-60:i,0])
+
+x_test = np.array(x_test)
+x_test.shape
+
+#%%
+x_test = np.reshape(x_test, (x_test.shape[0],x_test.shape[1],1))
+x_test.shape
+
+#%%
+predicted_price = regressor.predict(x_test)
+
+#%%
+predicted_price = sc.inverse_transform(predicted_price)
+predicted_price
+
+#%%
+plt.plot(real_stock_price,color = 'red', label = 'Real Price')
+plt.plot(predicted_price, color = 'blue', label = 'Predicted Price')
+plt.title('Google Stock Price Prediction')
+plt.xlabel('Time')
+plt.ylabel('Google Stock Price')
+plt.legend()
+plt.show()
