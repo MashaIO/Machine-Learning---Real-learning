429514fe87e1264834bc080d8d8c667c237ba7af
Venkatesh <venkat@masha.io>
2019-04-22 19:35:56 +0000

    Still there is a issue in reshaping.

--- dl-learnings/CNN/doodleclassifier.py
+++ dl-learnings/CNN/doodleclassifier.py
@@ -147,3 +147,72 @@ num_classes = 2
 
 #%%
 model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3),activation='linear',input_shape=(28,28,1),padding='same'))
+model.add(LeakyReLU(alpha=0.1))
+model.add(MaxPooling2D((2, 2),padding='same'))
+model.add(Conv2D(64, (3, 3), activation='linear',padding='same'))
+model.add(LeakyReLU(alpha=0.1))
+model.add(MaxPooling2D(pool_size=(2, 2),padding='same'))
+model.add(Conv2D(128, (3, 3), activation='linear',padding='same'))
+model.add(LeakyReLU(alpha=0.1))                  
+model.add(MaxPooling2D(pool_size=(2, 2),padding='same'))
+model.add(Flatten())
+model.add(Dense(128, activation='linear'))
+model.add(LeakyReLU(alpha=0.1))                  
+model.add(Dense(num_classes, activation='softmax'))
+
+#%%
+model.summary()
+model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(),metrics=['accuracy'])
+
+
+
+#%%
+
+#%%
+
+#%%
+total_training_data[0][1]
+
+#%%
+from sklearn.model_selection import train_test_split
+
+#%%
+print(type(total_training_data))
+print(total_training_data.shape)
+print(total_training_data.size)
+print(total_training_data[0])
+print(total_training_data[0][0])
+print(total_training_data[0][1])
+
+#%%
+train_X = []
+for x in range(1600):
+  train_X.append(total_training_data[x][0])
+
+#%%
+print(train_X[0])
+
+#%%
+train_Y_one_hot = []
+for x in range(1600):
+  train_Y_one_hot.append(total_training_data[x][1])
+
+#%%
+print(train_Y_one_hot)
+
+
+#%%
+#
+# train_X,valid_X,train_label,valid_label = train_test_split(train_X, train_Y_one_hot, test_size=0.2, random_state=13)
+#train_X_data,valid_X_data,train_label_data,valid_label_data = train_test_split(train_X, train_Y_one_hot, test_size=0.2, random_state=13)
+
+#%%
+train_X.reshape([-1,28, 28,1])
+
+
+
+#%%
+#model_train_dropout = model.fit(train_X, train_Y_one_hot, batch_size=batch_size,epochs=epochs,verbose=1,validation_data=(valid_X_data, valid_label_data))
+model_train_dropout = model.fit(np.array(train_X), np.array(train_Y_one_hot), batch_size = 50, epochs = 5, verbose = 1)
+
